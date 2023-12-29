939cd10276d1febf85bf2d9cd48d9fd8c96d01be
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-29 20:57:14 +0000

    tidying up

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,20 @@
 extern crate x11;
 use self::x11::xlib::_XDisplay;
 use std::{ptr, slice};
-pub struct KeypressDetect {
-  handle: *mut _XDisplay,
+
+pub fn get_display() -> *mut _XDisplay {
+  match unsafe { x11::xlib::XOpenDisplay(ptr::null()) } {
+    display if unsafe { display.as_ref().is_some() } => display,
+    _ => panic!("Could not connect to X display!"),
+  }
 }
 
-impl KeypressDetect {
-  pub fn get_display() -> KeypressDetect {
-    match unsafe { x11::xlib::XOpenDisplay(ptr::null()) } {
-      display if unsafe { display.as_ref().is_some() } => KeypressDetect { handle: display },
-      _ => panic!("Could not connect to a X display"),
-    }
+pub fn key_pressed(display: &*mut _XDisplay) -> bool {
+  let keymap = [0; 32].as_mut_ptr();
+  unsafe {
+    x11::xlib::XQueryKeymap(*display, keymap);
+    slice::from_raw_parts(keymap, 32)
   }
-
-  pub fn key_pressed(display: &KeypressDetect) -> bool {
-    let keymap = [0; 32].as_mut_ptr();
-    unsafe {
-      x11::xlib::XQueryKeymap(display.handle, keymap);
-      slice::from_raw_parts(keymap, 32)
-    }
-    .iter()
-    .any(|byte| *byte != 0)
-  }
+  .iter()
+  .any(|byte| *byte != 0)
 }
