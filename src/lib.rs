f314a6f2c4e89b434d9fe7cc1e3c61e93d1e5bcc
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-29 21:49:07 +0000

    tidying up

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-extern crate x11;
-use self::x11::xlib::_XDisplay;
-use std::{ptr, slice};
+use std::{ptr::null, slice::from_raw_parts};
+use x11::xlib::{XOpenDisplay, XQueryKeymap, _XDisplay};
 
 pub fn get_display() -> *mut _XDisplay {
-  match unsafe { x11::xlib::XOpenDisplay(ptr::null()) } {
+  match unsafe { XOpenDisplay(null()) } {
     display if unsafe { display.as_ref().is_some() } => display,
     _ => panic!("Could not connect to X display!"),
   }
@@ -12,8 +11,8 @@ pub fn get_display() -> *mut _XDisplay {
 pub fn key_pressed(display: &*mut _XDisplay) -> bool {
   let keymap = [0; 32].as_mut_ptr();
   unsafe {
-    x11::xlib::XQueryKeymap(*display, keymap);
-    slice::from_raw_parts(keymap, 32)
+    XQueryKeymap(*display, keymap);
+    from_raw_parts(keymap, 32)
   }
   .iter()
   .any(|byte| *byte != 0)
