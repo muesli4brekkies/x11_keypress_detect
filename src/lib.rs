a2f7299dd66161f39cbea47825d0fc8763d41e88
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-29 18:20:46 +0000

    struct name adjustments

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,18 @@
-use std::{
-  ptr, slice,
-};
+use std::{ptr, slice};
 use x11::xlib::{self, _XDisplay};
-pub struct DisplayHandle {
+pub struct KeypressDetect {
   handle: *mut _XDisplay,
 }
 
-impl DisplayHandle {
- pub  fn get_display() -> DisplayHandle {
+impl KeypressDetect {
+  pub fn get_display() -> KeypressDetect {
     match unsafe { xlib::XOpenDisplay(ptr::null()) } {
-      display if unsafe { display.as_ref().is_some() } => DisplayHandle { handle: display },
+      display if unsafe { display.as_ref().is_some() } => KeypressDetect { handle: display },
       _ => panic!("Could not connect to a X display"),
     }
   }
 
-pub  fn key_pressed(display: &DisplayHandle) -> bool {
+  pub fn key_pressed(display: &KeypressDetect) -> bool {
     let keymap = [0; 32].as_mut_ptr();
     unsafe {
       xlib::XQueryKeymap(display.handle, keymap);
